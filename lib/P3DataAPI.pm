5a535a1bc301ccaef5418b3b7ae2909e29ecdb93
Bruce Parrello <bparrello@figresearch.com>
2016-07-12 14:59:32 -0500

    Fixes for anomalies in PATRIC gtos.

--- lib/P3DataAPI.pm
+++ lib/P3DataAPI.pm
@@ -417,61 +417,65 @@ Returns a L<GenomeTypeObject> for the genome, or C<undef> if the genome was not 
 sub gto_of {
     my ($self, $genomeID) = @_;
     require GenomeTypeObject;
+    my $retVal;
     # Get the basic genome data.
     my ($g) = $self->query("genome", ["eq", "genome_id", $genomeID],
                     ["select", "genome_id", "genome_name", "genome_status", "taxon_id",  "taxon_lineage_names"],
                     );
-    # Compute the domain.
-    my $domain = $g->{taxon_lineage_names}[1];
-    if (! grep { $_ eq $domain} qw(Bacteria Archaea Eukaryota)) {
-        $domain = $g->{taxon_lineage_names}[0];
-    }
-    # Compute the genetic code.
-    my $genetic_code = 11; # Some day we need to fix this.
-    # Create the initial GTO.
-    my $retVal = GenomeTypeObject->new();
-    $retVal->set_metadata({ id => $g->{genome_id},
-                            scientific_name => $g->{genome_name},
-                            source => 'PATRIC',
-                            source_id => $g->{genome_id},
-                            ncbi_taxonomy_id => $g->{taxon_id},
-                            taxonomy => $g->{taxon_lineage_names},
-                            domain => $domain,
-                            genetic_code => $genetic_code
-    });
-    # Get the contigs.
-    my @contigs = $self->query("genome_sequence", ["eq", "genome_id", $genomeID],
-                    ["select", "sequence_id", "sequence"]);
-    my @gto_contigs;
-    for my $contig (@contigs) {
-        push @gto_contigs, { id => $contig->{sequence_id}, dna => $contig->{sequence},
-            genetic_code => $genetic_code };
-    }
-    $retVal->add_contigs(\@gto_contigs);
-    undef @contigs;
-    undef @gto_contigs;
-    # Get the features.
-    my @f = $self->query("genome_feature", ["eq", "genome_id", $genomeID],
-                    ["select", "patric_id", "sequence_id", "strand", "segments", "feature_type", "product", "aa_sequence"]);
-    # This prevents duplicates.
-    my %fids;
-    for my $f (@f) {
-        # Skip duplicates.
-        my $fid = $f->{patric_id};
-        if (! $fids{$fid}) {
-            my $prefix = $f->{sequence_id} . "_";
-            my $strand = $f->{strand};
-            my @locs;
-            for my $s (@{$f->{segments}}) {
-                my ($s1, $s2) = split /\.\./, $s;
-                my $len = $s2 + 1 - $s1;
-                my $start = ($strand eq '-' ? $s2 : $s1);
-                push @locs, "$prefix$start$strand$len";
+    # Only proceed if we found a genome.
+    if ($g) {
+        # Compute the domain.
+        my $domain = $g->{taxon_lineage_names}[1];
+        if (! grep { $_ eq $domain} qw(Bacteria Archaea Eukaryota)) {
+            $domain = $g->{taxon_lineage_names}[0];
+        }
+        # Compute the genetic code.
+        my $genetic_code = 11; # Some day we need to fix this.
+        # Create the initial GTO.
+        $retVal = GenomeTypeObject->new();
+        $retVal->set_metadata({ id => $g->{genome_id},
+                                scientific_name => $g->{genome_name},
+                                source => 'PATRIC',
+                                source_id => $g->{genome_id},
+                                ncbi_taxonomy_id => $g->{taxon_id},
+                                taxonomy => $g->{taxon_lineage_names},
+                                domain => $domain,
+                                genetic_code => $genetic_code
+        });
+        # Get the contigs.
+        my @contigs = $self->query("genome_sequence", ["eq", "genome_id", $genomeID],
+                        ["select", "sequence_id", "sequence"]);
+        my @gto_contigs;
+        for my $contig (@contigs) {
+            push @gto_contigs, { id => $contig->{sequence_id}, dna => $contig->{sequence},
+                genetic_code => $genetic_code };
+        }
+        $retVal->add_contigs(\@gto_contigs);
+        undef @contigs;
+        undef @gto_contigs;
+        # Get the features.
+        my @f = $self->query("genome_feature", ["eq", "genome_id", $genomeID],
+                        ["select", "patric_id", "sequence_id", "strand", "segments", "feature_type", "product", "aa_sequence"]);
+        # This prevents duplicates.
+        my %fids;
+        for my $f (@f) {
+            # Skip duplicates.
+            my $fid = $f->{patric_id};
+            if ($fid && ! $fids{$fid}) {
+                my $prefix = $f->{sequence_id} . "_";
+                my $strand = $f->{strand};
+                my @locs;
+                for my $s (@{$f->{segments}}) {
+                    my ($s1, $s2) = split /\.\./, $s;
+                    my $len = $s2 + 1 - $s1;
+                    my $start = ($strand eq '-' ? $s2 : $s1);
+                    push @locs, "$prefix$start$strand$len";
+                }
+                $retVal->add_feature({-id => $fid, -location => \@locs,
+                        -type => $f->{feature_type}, -function => $f->{product},
+                        -protein_translation => $f->{aa_sequence} });
+                $fids{$fid} = 1;
             }
-            $retVal->add_feature({-id => $fid, -location => \@locs,
-                    -type => $f->{feature_type}, -function => $f->{product},
-                    -protein_translation => $f->{aa_sequence} });
-            $fids{$fid} = 1;
         }
     }
     # Return the GTO.
