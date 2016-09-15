351821a558f22f49a9a6fc51c74a115efa0be2a8
Bruce Parrello <bparrello@figresearch.com>
2016-09-15 11:44:29 -0500

    Renamed extract to match. Changed extract to extract columns.

--- scripts/p3-extract.pl
+++ scripts/p3-extract.pl
@@ -1,32 +1,15 @@
-=head1 Select Rows from an Input File
+=head1 Select Columns from an Input File
 
-    p3-extract [options] value
+    p3-extract [options] col1 col2 ... colN
 
-This script extracts rows from a tab-delimited file based on the value in a specified column. Optionally,
-it can create a second output file containing the rejected rows.
+This script extracts the specified columns from a tab-delimited file.
 
 =head2 Parameters
 
-The single positional parameter is the value on which to match. If the value is numeric, the match will be
-exact. If it is non-numeric, then the match will be case-insensitive, and a record will match if any substring of
-the value in the key column matches the input value.
+The positional parameters are the numbers or names of the columns to include in the output. The column numbers
+are 1-based, and the column names are taken from the header record of the input file.
 
 The standard input may be overridden by the command-line options given in L<P3Utils/ih_options>.
-
-The command-line options are those in L<P3Utils/col_options> (for selection of the key column) plus the
-following.
-
-=over 4
-
-=item reverse
-
-If specified, only rows that do not match will be output.
-
-=item discards
-
-If specified, the name of a file to contain the records that do not match.
-
-=back
 
 =cut
 
@@ -34,38 +17,22 @@ use strict;
 use P3Utils;
 
 # Get the command-line options.
-my $opt = P3Utils::script_opts('match-value', P3Utils::ih_options(), P3Utils::col_options(),
-        ['reverse|v', 'output non-matching records'],
-        ['discards=s', 'name of file to contain discarded records']);
-# Get the reverse-flag.
-my $reverse = ($opt->reverse ? 1 : 0);
-# Get the match pattern.
-my ($pattern) = @ARGV;
-die "No match pattern specified." if (! $pattern);
+my $opt = P3Utils::script_opts('col1 col2 ... colN', P3Utils::ih_options());
+# Get the column identifiers.
+my @cols = @ARGV;
+die "No columns specified." if (! @cols);
 # Open the input file.
 my $ih = P3Utils::ih($opt);
-# Process the headers and compute the key column index.
-my ($outHeaders, $keyCol) = P3Utils::process_headers($ih, $opt);
-# Check for a discard file.
-my $dh;
-if ($opt->discards) {
-    open($dh, '>', $opt->discards) || die "Could not open discard file: $!";
-    P3Utils::print_cols($outHeaders, $dh);
-}
+# Process the headers.
+my ($inHeaders) = P3Utils::process_headers($ih);
+# Compute the column indices.
+my @idxes = map { P3Utils::find_column($_, $inHeaders) } @cols;
+# Compute the output headers.
+my @outHeaders = map { $inHeaders->[$_] } @idxes;
 # Write out the headers.
-P3Utils::print_cols($outHeaders);
+P3Utils::print_cols(\@outHeaders);
 # Loop through the input.
 while (! eof $ih) {
-    my $couplets = P3Utils::get_couplets($ih, $keyCol, $opt);
-    # Loop through the couplets.
-    for my $couplet (@$couplets) {
-        my ($key, $row) = @$couplet;
-        # Perform a match. If we have a match and are NOT reversing, or we do NOT have a match and are reversing, we output
-        # (XOR condition).
-        if (P3Utils::match($pattern, $key) ^ $reverse) {
-            P3Utils::print_cols($row);
-        } elsif ($dh) {
-            P3Utils::print_cols($row, $dh);
-        }
-    }
+    my @row =  P3Utils::get_cols($ih, \@idxes);
+    P3Utils::print_cols(\@row);
 }
