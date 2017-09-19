616b57a99392e08f3045d4caf3c442eb66c66596
Bruce Parrello <bparrello@figresearch.com>
2017-09-19 15:12:26 -0500

    Fixed kmer compare documentation.

--- scripts/p3-kmer-compare.pl
+++ scripts/p3-kmer-compare.pl
@@ -3,8 +3,11 @@
     p3-kmer-compare.pl [options] genome1 genome2 ... genomeN
 
 This script compares genomes based on DNA kmers. It outputs the number of kmers the two genomes have in common, the number appearing
-only in the first genome, and the number appearing only in the second. It then produces a ratio from 0 to 1 indicating the DNA similarity.
-A value of 1 indicates the genomes are effectively identical.
+only in the first genome, and the number appearing only in the second. It then produces a pair of percentages for each combination-- completeness and
+contamination-- displayed in a matrix. The percentages project the row genome onto the column genome. A completeness of 100% means every kmer in the
+column genome is found in the row genome. A contamination of 100% means every kmer in the column genome is NOT found in the row genome. So, if the
+genomes are identical, the percentages will be C<100.0/0.0>. If the column genome is a subset of the row genome, the completeness will be 100% but
+the contamination will be nonzero.
 
 =head2 Parameters
 
