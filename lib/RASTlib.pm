1a3bd2f456b30603426da4331de15bb97b57de77
Bruce Parrello <bparrello@figresearch.com>
2019-01-28 21:19:56 -0600

    Support for better batch processing.

--- lib/RASTlib.pm
+++ lib/RASTlib.pm
@@ -84,6 +84,11 @@ The domain for the genome (C<A>, C<B>, ...). The default is C<B>, for bacteria.
 
 The genetic code for protein translation. The default is C<11>.
 
+=item path
+
+The name for the output folder in the user's workspace to contain the job.  If omitted, the default folder
+C<QuickData> is used.  The path must be fully-formed, with a leading slash and the user name included.
+
 =item sleep
 
 The sleep interval in seconds while waiting for RAST to complete. The default is C<60>.
@@ -100,6 +105,91 @@ Returns an unblessed L<GenomeTypeObject> for the annotated genome.
 
 sub Annotate {
     my ($contigs, $taxonID, $name, %options) = @_;
+    # Get the options.
+    my $sleepInterval = $options{sleep} || 60;
+    my $user = $options{user};
+    # This will contain the return value.
+    my $retVal;
+    # Create the authorization header.
+    my $header = auth_header($options{user}, $options{password});
+    # Submit the job.
+    my $jobID = Submit($contigs, $taxonID, $name, %options, header => $header);
+    warn "Rast job ID is $jobID.\n";
+    # Begin spinning for a completion status.
+    while (! check($jobID, $header)) {
+        sleep $sleepInterval;
+    }
+    # Get the results.
+    $retVal = retrieve($jobID, $header);
+    # Return the GTO built.
+    return $retVal;
+}
+
+=head3 Submit
+
+    my $jobID = RASTlib::Submit(\@contigs, $taxonID, $name, %options);
+
+Submit an annotation to RAST and return the job ID.
+
+=over 4
+
+=item contigs
+
+Reference to a list of 3-tuples containing the contigs. Each 3-tuple contains (0) a contig ID, (1) a comment,
+and (2) the DNA sequence.
+
+=item genomeID
+
+The taxonomic ID for the genome.
+
+=item name
+
+The scientific name for the genome.
+
+=item options
+
+A hash containing zero or more of the following options.
+
+=over 8
+
+=item user
+
+The RAST user name.  If omitted, a stored value is found in a configuration file.
+
+=item password
+
+The RAST password.  If omitted, a stored value is found in a configuration file.
+
+=item domain
+
+The domain for the genome (C<A>, C<B>, ...). The default is C<B>, for bacteria.
+
+=item geneticCode
+
+The genetic code for protein translation. The default is C<11>.
+
+=item path
+
+The name for the output folder in the user's workspace to contain the job.  If omitted, the default folder
+C<QuickData> is used.  The path must be fully-formed, with a leading slash and the user name included.
+
+=item header
+
+If specified, an authorization header containing the user's credentials.  In this case, the B<user> and
+B<password> options are ignored.
+
+=back
+
+=item RETURN
+
+Returns an unblessed L<GenomeTypeObject> for the annotated genome.
+
+=back
+
+=cut
+
+sub Submit {
+    my ($contigs, $taxonID, $name, %options) = @_;
     if (! $taxonID) {
         die "Missing taxon ID for RAST annotation.";
     } elsif ($taxonID =~ /^(\d+)\.\d+$/) {
@@ -110,14 +200,14 @@ sub Annotate {
     if (! $name) {
         die "No genome name specified for RAST annotation.";
     }
+    # This will be the return value.
+    my $retVal;
     # Get the options.
     my $user = $options{user};
     my $pass = $options{password};
     my $domain = $options{domain} // 'B';
     my $geneticCode = $options{geneticCode} // 11;
-    my $sleepInterval = $options{sleep} || 60;
-    # This will contain the return value.
-    my $retVal;
+    my $path = $options{path};
     # Create the contig string.
     my $contigString = join("", map { ">$_->[0] $_->[1]\n$_->[2]\n" } @$contigs );
     # Fix up the name.
@@ -125,76 +215,32 @@ sub Annotate {
         $name = "Unknown sp. $name";
     }
     # Now we create an HTTP request to submit the job to RAST.
-    my $url = URI->new(RAST_URL . '/submit/GenomeAnnotation');
-    $url->query_form(
-        scientific_name => $name,
+    my %parms = (scientific_name => $name,
         taxonomy_id => $taxonID,
         genetic_code => $geneticCode,
-        domain => $domain
+        domain => $domain,
     );
-    my $header = HTTP::Headers->new(Content_Type => 'text/plain');
-    if (! $user || ! $pass) {
-        my $passfile = $P3DataAPI::token_path . "-code";
-        open(my $ph, "<$P3DataAPI::token_path") || die "User not logged in: $!";
-        my $token = <$ph>;
-        if ($token =~ /un=([^|]+)\@patricbrc\.org/) {
-            $user = $1;
-        } else {
-            die "Invalid format in login token.";
-        }
-        undef $ph;
-        open($ph, "<$passfile") || die "User not logged in: $!";
-        my $unpacked = <$ph>;
-        my $encrypted = pack('H*', $unpacked);
-        my $ref = Crypt::RC4->new($user);
-        $pass = $ref->RC4($encrypted);
+    if ($path) {
+        $parms{path} = $path;
     }
-    my $userURI = "$user\@patricbrc.org";
-    $header->authorization_basic($userURI, $pass);
-    my $request = HTTP::Request->new(POST => "$url", $header, $contigString);
+    my $url = URI->new(RAST_URL . '/submit/GenomeAnnotation');
+    $url->query_form(%parms);
+    # Create the authorization header.
+    my $header = $options{header} // auth_header($user, $pass);
     # Submit the request.
+    my $request = HTTP::Request->new(POST => "$url", $header, $contigString);
     my $ua = LWP::UserAgent->new();
     my $response = $ua->request($request);
     if ($response->code ne 200) {
         die "Error response for RAST submisssion: " . $response->message;
     } else {
         # Get the job ID.
-        my $jobID = $response->content;
-        warn "Rast job ID is $jobID.\n";
-        # Form a request for retreiving the job status.
-        $url = join("/", RAST_URL, $jobID, 'status');
-        $request = HTTP::Request->new(GET => $url, $header);
-        # Begin spinning for a completion status.
-        my $done;
-        while (! $done) {
-            sleep $sleepInterval;
-            $response = $ua->request($request);
-            if ($response->code ne 200) {
-                die "Error response for RAST status: " . $response->message;
-            } else {
-                 my $status = $response->content;
-                 if ($status eq 'completed') {
-                     $done = 1;
-                 } elsif ($status ne 'in-progress' && $status ne 'queued') {
-                     die "Error status for RAST: $status.";
-                 }
-            }
-        }
-        # Get the results.
-        $url = join("/", RAST_URL, $jobID, 'retrieve');
-        $request = HTTP::Request->new(GET => $url, $header);
-        $response = $ua->request($request);
-        if ($response->code ne 200) {
-            die "Error response for RAST retrieval: " . $response->message;
-        }
-        my $json = $response->content;
-        $retVal = SeedUtils::read_encoded_object(\$json);
-        # Add the RAST information to the GTO.
-        $retVal->{rast_specs} = { id => $jobID, user => $user }
+        $retVal = $response->content;
     }
-    # Return the GTO built.
+    # Return the job ID.
     return $retVal;
 }
+
 
 =head3 check
 
@@ -210,7 +256,7 @@ ID of the job to check.
 
 =item user
 
-The RAST user name.
+The RAST user name, or alternatively, a pre-computed authorization header from L</auth_header>.
 
 =item password
 
@@ -228,10 +274,13 @@ sub check {
     my ($jobID, $user, $pass) = @_;
     # This will be the return value.
     my $retVal = 0;
-    # Create an authorization header.
-    my $header = HTTP::Headers->new(Content_Type => 'text/plain');
-    my $userURI = "$user\@patricbrc.org";
-    $header->authorization_basic($userURI, $pass);
+    # Check for an authorization header.
+    my $header = $user;
+    if (ref $header ne 'HTTP::Headers') {
+        $header = HTTP::Headers->new(Content_Type => 'text/plain');
+        my $userURI = "$user\@patricbrc.org";
+        $header->authorization_basic($userURI, $pass);
+    }
     # Form a request for retreiving the job status.
     my $url = join("/", RAST_URL, $jobID, 'status');
     my $request = HTTP::Request->new(GET => $url, $header);
@@ -250,6 +299,105 @@ sub check {
          }
     }
     # Return the status.
+    return $retVal;
+}
+
+
+=head3 retrieve
+
+    my $gto = RASTlib::retrieve($jobID, $header);
+
+Extract the GTO for an annotated genome from the RAST interface.
+
+=over 4
+
+=item jobID
+
+The ID of the job that annotated the genome.
+
+=item header
+
+The authorization header returned by L</auth_header>.
+
+=item RETURN
+
+Returns an unblessed L<GenomeTypeObject> for the annotated genome.
+
+=back
+
+=cut
+
+sub retrieve {
+    my ($jobID, $header) = @_;
+    # Ask for the GTO from PATRIC.
+    my $ua = LWP::UserAgent->new();
+    my $url = join("/", RAST_URL, $jobID, 'retrieve');
+    my $request = HTTP::Request->new(GET => $url, $header);
+    my $response = $ua->request($request);
+    if ($response->code ne 200) {
+        die "Error response for RAST retrieval: " . $response->message;
+    }
+    my $json = $response->content;
+    my $retVal = SeedUtils::read_encoded_object(\$json);
+    # Add the RAST information to the GTO.
+    my ($userH) = $header->authorization_basic();
+    if ($userH =~ /^(.+)\@patricbrc.org/) {
+        $userH = $1;
+    }
+    $retVal->{rast_specs} = { id => $jobID, user => $userH };
+    # Return it.
+    return $retVal;
+}
+
+
+=head3 auth_header
+
+    my $header = auth_header($user, $pass);
+
+Create an authorization header for HTTP requests from the specified user ID and password.
+
+=over 4
+
+=item user
+
+The RAST user name.  If omitted, a stored value is found in a configuration file.
+
+=item password
+
+The RAST password.  If omitted, a stored value is found in a configuration file.
+
+=item RETURN
+
+Returns an L<HTTP::Headers> object with the authorization credentials built in.
+
+=back
+
+=cut
+
+sub auth_header {
+    my ($user, $pass) = @_;
+    my $retVal = HTTP::Headers->new(Content_Type => 'text/plain');
+    if (! $user || ! $pass) {
+        # If the user ID and password were not passed in, use the stored information.
+        my $passfile = $P3DataAPI::token_path . "-code";
+        open(my $ph, "<$P3DataAPI::token_path") || die "User not logged in: $!";
+        my $token = <$ph>;
+        if ($token =~ /un=([^|]+)\@patricbrc\.org/) {
+            $user = $1;
+        } else {
+            die "Invalid format in login token.";
+        }
+        undef $ph;
+        open($ph, "<$passfile") || die "User not logged in: $!";
+        my $unpacked = <$ph>;
+        my $encrypted = pack('H*', $unpacked);
+        my $ref = Crypt::RC4->new($user);
+        $pass = $ref->RC4($encrypted);
+    }
+    # Create the header.
+    my $userURI = "$user\@patricbrc.org";
+    $retVal->authorization_basic($userURI, $pass);
+    # Return the header.
     return $retVal;
 }
 
